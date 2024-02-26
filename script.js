d39c8a2cedac790026f97680fe81fb4bcb1b01fb
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-26 18:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const panals=document.querySelectorAll('.panel');
+  panels.forEach(panel=>{
+	  panal.addEventlistener('click',()=>{
+	  removeActiveClasses();
+	  panel.classList.add('active');
+  })
+	  
+  })
+function removeActiveClasses() {
+	 panels.forEach(panel=>{
+		  panel.classList.remove('active');
+}
+					
